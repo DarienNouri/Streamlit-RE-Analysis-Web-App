b45133ee88af282fb4b3598d1e2bff2ea9c4f5f2
DarienNouri <106352088+DarienNouri@users.noreply.github.com>
2022-10-24 19:59:43 -0400

    Add files via upload

---
+++ plotlyFunctions.py
@@ -0,0 +1,153 @@
+import numpy as np
+import plotly.figure_factory as ff
+import plotly.offline as py_offline
+import plotly.express as px
+
+def zoom_center(lons: tuple=None, lats: tuple=None, lonlats: tuple=None,
+        format: str='lonlat', projection: str='mercator',
+        width_to_height: float=2.0) -> (float, dict):
+    """Finds optimal zoom and centering for a plotly mapbox.
+    Must be passed (lons & lats) or lonlats.
+    Temporary solution awaiting official implementation, see:
+    https://github.com/plotly/plotly.js/issues/3434
+
+    Parameters
+    --------
+    lons: tuple, optional, longitude component of each location
+    lats: tuple, optional, latitude component of each location
+    lonlats: tuple, optional, gps locations
+    format: str, specifying the order of longitud and latitude dimensions,
+        expected values: 'lonlat' or 'latlon', only used if passed lonlats
+    projection: str, only accepting 'mercator' at the moment,
+        raises `NotImplementedError` if other is passed
+    width_to_height: float, expected ratio of final graph's with to height,
+        used to select the constrained axis.
+
+    Returns
+    --------
+    zoom: float, from 1 to 20
+    center: dict, gps position with 'lon' and 'lat' keys
+
+    >>> print(zoom_center((-109.031387, -103.385460),
+    ...     (25.587101, 31.784620)))
+    (5.75, {'lon': -106.208423, 'lat': 28.685861})
+    """
+    if lons is None and lats is None:
+        if isinstance(lonlats, tuple):
+            lons, lats = zip(*lonlats)
+        else:
+            raise ValueError(
+                'Must pass lons & lats or lonlats'
+            )
+
+    maxlon, minlon = max(lons), min(lons)
+    maxlat, minlat = max(lats), min(lats)
+    center = {
+        'lon': round((maxlon + minlon) / 2, 6),
+        'lat': round((maxlat + minlat) / 2, 6)
+    }
+
+    # longitudinal range by zoom level (20 to 1)
+    # in degrees, if centered at equator
+    lon_zoom_range = np.array([
+        0.0007, 0.0014, 0.003, 0.006, 0.012, 0.024, 0.048, 0.096,
+        0.192, 0.3712, 0.768, 1.536, 3.072, 6.144, 11.8784, 23.7568,
+        47.5136, 98.304, 190.0544, 360.0
+    ])
+
+    if projection == 'mercator':
+        margin = 1.2
+        height = (maxlat - minlat) * margin * width_to_height
+        width = (maxlon - minlon) * margin
+        lon_zoom = np.interp(width , lon_zoom_range, range(20, 0, -1))
+        lat_zoom = np.interp(height, lon_zoom_range, range(20, 0, -1))
+        zoom = round(min(lon_zoom, lat_zoom), 2)
+    else:
+        raise NotImplementedError(
+            f'{projection} projection is not implemented'
+        )
+
+    return zoom+6, center
+#zoom, center = zoom_center(lons= lon, lats= lat)
+
+
+def makeHeatmap(df):
+
+
+    dfForCorr = df[['unformattedPrice','zestimate','taxAssessedValue','addressZipcode','beds', 'baths', 'area','latitude', 'longitude']]
+    corr = dfForCorr.corr().round(3)
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    df_mask = corr.mask(mask)
+
+    fig = ff.create_annotated_heatmap(z=df_mask.to_numpy(),
+                                      x=df_mask.columns.tolist(),
+                                      y=df_mask.columns.tolist(),
+                                      colorscale=px.colors.sequential.Inferno,
+                                      hoverinfo="none",  # Shows hoverinfo for null values
+                                      showscale=True, ygap=1, xgap=1
+                                      )
+
+    fig.update_xaxes(side="bottom")
+
+    fig.update_layout(
+        title_text='Heatmap',
+        title_x=0.5,
+
+        height = 800,
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+        xaxis_zeroline=False,
+        yaxis_zeroline=False,
+        yaxis_autorange='reversed',
+        template='plotly_white'
+    )
+
+    # NaN values are not handled automatically and are displayed in the figure
+    # So we need to get rid of the text manually
+    for i in range(len(fig.layout.annotations)):
+        if fig.layout.annotations[i].text == 'nan':
+            fig.layout.annotations[i].text = ""
+
+    return fig
+
+def get_plotting_zoom_level_and_center_coordinates_from_lonlat_tuples(longitudes=None, latitudes=None):
+    """Function documentation:\n
+    Basic framework adopted from Krichardson under the following thread:
+    https://community.plotly.com/t/dynamic-zoom-for-mapbox/32658/7
+
+    # NOTE:
+    # THIS IS A TEMPORARY SOLUTION UNTIL THE DASH TEAM IMPLEMENTS DYNAMIC ZOOM
+    # in their plotly-functions associated with mapbox, such as go.Densitymapbox() etc.
+
+    Returns the appropriate zoom-level for these plotly-mapbox-graphics along with
+    the center coordinate tuple of all provided coordinate tuples.
+    """
+
+    # Check whether both latitudes and longitudes have been passed,
+    # or if the list lenghts don't match
+    if ((latitudes is None or longitudes is None)
+            or (len(latitudes) != len(longitudes))):
+        # Otherwise, return the default values of 0 zoom and the coordinate origin as center point
+        return 0, (0, 0)
+
+    # Get the boundary-box
+    b_box = {}
+    b_box['height'] = latitudes.max()-latitudes.min()
+    b_box['width'] = longitudes.max()-longitudes.min()
+    b_box['center']= (np.mean(longitudes), np.mean(latitudes))
+
+    # get the area of the bounding box in order to calculate a zoom-level
+    area = b_box['height'] * b_box['width']
+
+    # * 1D-linear interpolation with numpy:
+    # - Pass the area as the only x-value and not as a list, in order to return a scalar as well
+    # - The x-points "xp" should be in parts in comparable order of magnitude of the given area
+    # - The zpom-levels are adapted to the areas, i.e. start with the smallest area possible of 0
+    # which leads to the highest possible zoom value 20, and so forth decreasing with increasing areas
+    # as these variables are antiproportional
+    zoom = np.interp(x=area,
+                     xp=[0, 5**-10, 4**-10, 3**-10, 2**-10, 1**-10, 1**-5],
+                     fp=[20, 15,    14,     13,     12,     7,      5])
+
+    # Finally, return the zoom level and the associated boundary-box center coordinates
+    return zoom, b_box['center']
